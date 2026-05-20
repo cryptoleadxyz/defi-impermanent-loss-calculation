f80b1146c9c383a0e0d315f87235faf09fd3fb8e
CryptoLead <103460389+cryptoleadxyz@users.noreply.github.com>
2026-05-20 11:12:54 -0600

    Clean up comments in impermanent loss calculator
    Removed author information and donation request comments.

--- 3_impermanent_loss_calculator_balancer.py
+++ 3_impermanent_loss_calculator_balancer.py
@@ -1,8 +1,3 @@
-# Author: CryptoLead at https://www.cryptolead.xyz
-# Date: 2023-04-17
-# Message: Hey there fellow coders! If you found my code helpful and want to show your support, consider buying me a coffee or two. Your donations help me keep improving the code and creating more awesome stuff for the community. Thanks for your support!
-# Donation: cryptolead.eth or 0xa2c35DA418f52ed89Ba18d51DbA314EB1dc396d0
-
 # Objectives:
 # Previous examples only deal with impermanent loss calculation with equal (or 50:50) token pair. In Balancer, token pairs can have different proportion and can have more than 2 tokens, this .py derives the "flexible" impermanent loss calculation based on the formulas of the following link
 # Reference URL = https://chainbulletin.com/impermanent-loss-explained-with-examples-math
